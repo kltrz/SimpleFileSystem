f0b7d918b4dae713957cf6edf83f27948554d0d8
kltrz <kltrz92@gmail.com>
2018-12-17 20:26:39 +0100

    Tutte le funzioni implementate

--- bitmap.c
+++ bitmap.c
@@ -1,15 +1,8 @@
-#pragma once
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "bitmap.h"
 
-typedef struct{
-  int num_bits;
-  char* entries;
-}  BitMap;
-
-typedef struct {
-  int entry_num;
-  char bit_num;
-} BitMapEntryKey;
 
 void chartobin(char c, char r[])
 { 
@@ -28,6 +21,8 @@ BitMapEntryKey BitMap_blockToIndex(int num){
 	return ret;
 }
 
+
+
 // converts a bit to a linear index
 int BitMap_indexToBlock(int entry, unsigned char bit_num){
 	int ret = entry * 8;
@@ -38,41 +33,66 @@ int BitMap_indexToBlock(int entry, unsigned char bit_num){
 // returns the index of the first bit having status "status"
 // in the bitmap bmap, and starts looking from position start
 int BitMap_get(BitMap* bmap, int start, int status){
+	/*poner que el indice no sea mayor de la dimension de la bimap (HACER UN CONTROL)*/
+	
+	/*if(start > bmlen){
+		printf("Errore: limite di posizione passato\n");
+		exit(1);
+	}*/
+	int bmlen = ((*bmap).num_bits)/8;
 	char bitsofchar[8];
-	char stat = (char) status;
+	char stat = status + '0';
 	BitMapEntryKey bmkey = BitMap_blockToIndex(start);
-	char c = bmap.entries[bmkey.entry_num];
+	char c = (*bmap).entries[bmkey.entry_num];
 	chartobin(c, bitsofchar);
-	int len = strlen(bitsofchar);
-	int bmlen = strlen(bmap.entries);
+	int len =strlen(bitsofchar);
 	int counter = bmkey.entry_num+1;
-	for(int i = bmkey.bit_num; i < len-bmap.bit_num; i++){
+	for(int i = bmkey.bit_num; i < len-bmkey.bit_num; i++){
 		if(bitsofchar[i] == stat){
-		return start+i;
+		return BitMap_indexToBlock(start,i);
 	}
 }
 	while(counter < bmlen){
-	 c = bmap.entries[counter];
+	 c = (*bmap).entries[counter];
 	 chartobin(c, bitsofchar);
-	 for(i = 0; i<len; i++){
+	 for(int i = 0; i<len; i++){
 	 	if(bitsofchar[i] == stat){
-		return counter+i;
+		return BitMap_indexToBlock(counter,i);
 	    }
-	
+	}
+	counter++;
 }
-}
+return -1;
 }
 // sets the bit at index pos in bmap to status
 int BitMap_set(BitMap* bmap, int pos, int status){
-
+	/*poner que el indice no sea mayor de la dimension de la bimap (HACER UN CONTROL)*/
 	char bitsofchar[8];
-	char stat = (char) status;
+	char stat = status +'0';
 	BitMapEntryKey bmkey = BitMap_blockToIndex(pos);
-	char c = bmap.entries[bmkey.entry_num];
+	char c = (*bmap).entries[bmkey.entry_num];
 	chartobin(c, bitsofchar);
 	bitsofchar[bmkey.bit_num] = stat;
-	bmap.entries[bmkey.entry_num] = strtol(bitsofchar, 0, 2);
+	(*bmap).entries[bmkey.entry_num] = strtol(bitsofchar, 0, 2);
 	return pos;
 
+}
+
+void Stampa_BitMap(BitMap* bmap){
+	int bitmaplen = ((*bmap).num_bits)/8;
+	int counter = 0;
+	char bitsofchar[8];
+	char c;
+	int len =8;
+	while(counter < bitmaplen){
+	 c = (*bmap).entries[counter];
+	 chartobin(c, bitsofchar);
+	 for(int i = 0; i<len; i++){
+	 	printf("%c", bitsofchar[i]);
+	    }
+	    printf("\n");
+	    counter++;
+	}
+	
 }
 
